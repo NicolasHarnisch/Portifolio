edfb239af7da20495bce3b59e9aa61a59bacd098
NicolasHarnisch <nicolasgomeshar@gmail.com>
2026-04-06 16:13:50 -0300

    mudança numero novo

--- src/components/WhatsAppButton.tsx
+++ src/components/WhatsAppButton.tsx
@@ -6,7 +6,7 @@ const WhatsAppButton = () => {
 
   return (
     <a
-      href="https://wa.me/558599997395"
+      href="https://wa.me/5585999973965"
       target="_blank"
       rel="noopener noreferrer"
       className="md:hidden fixed bottom-6 right-6 z-50 p-4 rounded-full bg-[#25D366] text-white shadow-lg hover:scale-110 transition-transform duration-300 glow animate-in fade-in zoom-in"
